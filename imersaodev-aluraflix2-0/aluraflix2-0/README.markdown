b6c74d629d6a630be5494f0358714dce1817593f
Mariana Izumi <81810638+MariMiks@users.noreply.github.com>
2021-12-02 11:16:30 -0300

    Adicionando projeto Aluraflix 2.0

---
+++ imersaodev-aluraflix2-0/aluraflix2-0/README.markdown
@@ -0,0 +1,5 @@
+# Aluraflix2.0
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Miksana/pen/PoWZMXV](https://codepen.io/Miksana/pen/PoWZMXV).
+
+
---
+++ imersaodev-aluraflix2-0/aluraflix2-0/src/script.js
@@ -0,0 +1,17 @@
+function adicionarFilme() {
+  var campoFilmeFavorito = document.querySelector ('#filme')
+  var filmeFavorito = campoFilmeFavorito.value
+  if (filmeFavorito.startsWith('https')){
+   listarFilmesNaTela(filmeFavorito)
+  } else {
+    alert("Vídeo Inválido")
+  }
+  campoFilmeFavorito.value = ""
+}
+
+function listarFilmesNaTela(filme) {
+  var listaFilmes = document.querySelector('#listaFilmes')
+  var elementoFilme = "<iframe src=" + filme + ">"
+  listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
+}
+
